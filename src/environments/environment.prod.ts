c645b57fbeadefdc9442e57cd12b1c5c77d3e50a
Jeff Hansen <dscheffy@gmail.com>
2019-02-15 20:45:46 +0000

    Separate prod and staging environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,5 @@
 export const environment = {
   production: true,
   baseFoodImageUrl: "http://www.feedmejefferson.com/images/",
-  appetiteUrl: "https://us-central1-feedme-stage.cloudfunctions.net/appetite",
-  firebase: {
-    apiKey: "AIzaSyCtjvY1oF7VFBpq1rWDt6bjfYKr1_OiHMk",
-    authDomain: "feedme-stage.firebaseapp.com",
-    databaseURL: "https://feedme-stage.firebaseio.com",
-    projectId: "feedme-stage",
-    storageBucket: "feedme-stage.appspot.com",
-    messagingSenderId: "448474045438"
-  }
+  appetiteUrl: "https://us-central1-feedme-jefferson.cloudfunctions.net/appetite"
 };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  baseFoodImageUrl: "http://www.feedmejefferson.com/images/",
+  appetiteUrl: "https://us-central1-feedme-stage.cloudfunctions.net/appetite",
+  firebase: {
+    apiKey: "AIzaSyCtjvY1oF7VFBpq1rWDt6bjfYKr1_OiHMk",
+    authDomain: "feedme-stage.firebaseapp.com",
+    databaseURL: "https://feedme-stage.firebaseio.com",
+    projectId: "feedme-stage",
+    storageBucket: "feedme-stage.appspot.com",
+    messagingSenderId: "448474045438"
+  }
+};
