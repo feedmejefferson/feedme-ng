5ccc8e05f3015abdeaead265045eb835643adbd7
Jeff Hansen <dscheffy@gmail.com>
2019-02-20 20:28:14 +0000

    Create second staging environment hosted on github

---
+++ src/environments/environment.github.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  baseFoodUrl: "https://feedmejefferson.github.io/images/",
+  appetiteUrl: "https://us-central1-feedme-stage.cloudfunctions.net/appetite"
+};
