ac86d3b2f930b86ebdc06c76404d72e951f03a14
Jeff Hansen <dscheffy@gmail.com>
2019-04-26 10:35:33 +0200

    Fix auth issue
    The auth service hasn't been updating the user document in firestore for
    new users (probably since we switched over to using redirects instead of
    popups). We didn't notice this until a new user came along and tried
    logging in. He was logged in, but couldn't see it because the update
    user logic wasn't happening and the visual feedback was based on the
    firestore user rather than just using the auth service user object. We
    aren't yet at a point where we necessarily want users to have profile
    pages that other users can see, so there's no real reason to be saving
    the users profile in firestore. All we need for moderation is to copy
    over the users uid into the moderator collection.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,10 +4,8 @@ import { User } from '../models/user.model';
 
 import { auth } from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 
-import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -16,43 +14,16 @@ export class AuthService {
 
     constructor(
         private afAuth: AngularFireAuth,
-        private afs: AngularFirestore,
         private router: Router
     ) { 
-      // Get the auth state, then fetch the Firestore user document or return null
-      this.user$ = this.afAuth.authState.pipe(
-        switchMap(user => {
-            // Logged in
-          if (user) {
-            return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
-          } else {
-            // Logged out
-            return of(null);
-          }
-        })
-      )
+      this.user$ = this.afAuth.authState;
     }
 
     async googleSignin() {
       const provider = new auth.GoogleAuthProvider();
-      const credential = await this.afAuth.auth.signInWithRedirect(provider);
-  //    return this.updateUserData(credential.user);
+      this.afAuth.auth.signInWithRedirect(provider);
     }
   
-    private updateUserData(user) {
-      // Sets user data to firestore on login
-      const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
-  
-      const data = { 
-        uid: user.uid, 
-        email: user.email, 
-        displayName: user.displayName, 
-        photoURL: user.photoURL
-      } 
-  
-      return userRef.set(data, { merge: true })
-  
-    }
   
     async signOut() {
       await this.afAuth.auth.signOut();
